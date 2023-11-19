f144c95f1a74fa7ba2b3cd185d2f9b8305b3510a
klartz <kurtzlara00@gmail.com>
2023-11-19 14:28:08 -0300

    feat: implement Cart component and state management

---
+++ prueba-tecnica/src/components/Cart/Cart.jsx
@@ -0,0 +1,71 @@
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart } from '../../redux/appActions';
+import ProductCount from '../ProductCount/ProductCount';
+
+const Cart = () => {
+	const dispatch = useDispatch();
+	const selectedProducts = useSelector((state) => state.cart.products);
+
+	return (
+		<TableContainer component={Paper}>
+			<Table sx={{minWidth: 650}} aria-label='simple table'>
+				<TableHead>
+					<TableRow>
+						<TableCell align='center' style={{fontSize: '2em'}}>
+							Cantidad
+						</TableCell>
+						<TableCell align='center' style={{fontSize: '2em'}}>
+							Descripción
+						</TableCell>
+						<TableCell align='center' style={{fontSize: '2em'}}>
+							Subtotal&nbsp;
+						</TableCell>
+						<TableCell align='center' style={{fontSize: '2em'}}>
+							Total&nbsp;
+						</TableCell>
+						<TableCell align='center' style={{fontSize: '2em'}}>
+							&nbsp;
+						</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{selectedProducts?.map((product) => (
+						<TableRow
+							key={product.name}
+							sx={{'&:last-child td, &:last-child th': {border: 0}}}
+						>
+							<TableCell align='center'>
+								<ProductCount product={product} />
+							</TableCell>
+							<TableCell
+								align='center'
+								component='th'
+								scope='product'
+								onClick={() => {
+									dispatch(removeFromCart(product));
+								}}
+							>
+								{product.name}
+							</TableCell>
+							<TableCell align='right'>{product.price}</TableCell>
+							<TableCell align='right'>
+								{product.price * product.count}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+};
+
+export default Cart;
---
+++ prueba-tecnica/src/slices/cartSlice.js
@@ -0,0 +1,50 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+	products: [], // array of objects {product, price, count}
+};
+
+const cartSlice = createSlice({
+	name: 'cart',
+	initialState,
+	reducers: {
+		/*
+			if product exists in state.products, increment count by 1
+			if not, add product to state.products with count = 1
+		*/
+		addToCart: (state, action) => {
+			console.log('action desde reducer', action);
+			console.log('state desde reducer', state.products);
+			const product = action.payload;
+			const productIndex = state.products.findIndex(
+				(stateProduct) => stateProduct.name === product.name
+			);
+			if (productIndex === -1) {
+				state.products = [...state.products, {...product, count: 1}];
+			} else {
+				state.products[productIndex].count += 1;
+			}
+		},
+
+		/*
+			if there is more than one instance of the product in state.products,
+			decrement count by 1
+			if not, remove product from state.products
+		*/
+		removeFromCart: (state, action) => {
+			const product = action.payload;
+			const productIndex = state.products.findIndex(
+				(stateProduct) => stateProduct.name === product.name
+			);
+			if (productIndex !== -1) {
+				state.products[productIndex].count -= 1;
+				if (state.products[productIndex].count === 0) {
+					state.products.splice(productIndex, 1);
+				}
+			}
+		},
+	},
+});
+
+export const {addToCart, removeFromCart} = cartSlice.actions;
+export default cartSlice.reducer;
