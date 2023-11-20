d3c4f6c0a5b7859de8f530b4767a710c111f6c7a
klartz <kurtzlara00@gmail.com>
2023-11-20 02:09:51 -0300

    feat: modify Home styling

--- prueba-tecnica/src/Home.jsx
+++ prueba-tecnica/src/Home.jsx
@@ -1,32 +1,35 @@
-import { default as Grid, default as Item } from '@mui/material/Grid';
+import {default as Grid} from '@mui/material/Grid';
 // eslint-disable-next-line no-unused-vars
 import * as React from 'react';
 import Cart from './components/Cart/Cart';
 import ProductList from './components/ProductList/ProductList';
 import './Home.css';
 
+const bottomGridItemProps = {
+	xs: 6,
+	md: 4,
+	height: '50vh',
+	align: 'center',
+};
+
 const Home = () => {
 	return (
 		<Grid container spacing={2} columns={12}>
 			<Grid
-				className='item-1'
 				item
 				xs={6}
 				lg={12}
 				width={'100vw'}
 				height={'50vh'}
+				align={'center'}
 			>
 				<Cart />
 			</Grid>
-			<Grid className='item-2' item xs={6} md={4} height={'50vh'}>
+			<Grid {...bottomGridItemProps}>
 				<ProductList />
 			</Grid>
-			<Grid className='item-3' item xs={6} md={4} height={'50vh'}>
-				<Item>xs=6 md=4</Item>
-			</Grid>
-			<Grid className='item-4' item xs={6} md={4} height={'50vh'}>
-				<Item>xs=6 md=4</Item>
-			</Grid>
+			<Grid {...bottomGridItemProps}></Grid>
+			<Grid {...bottomGridItemProps}></Grid>
 		</Grid>
 	);
 };
